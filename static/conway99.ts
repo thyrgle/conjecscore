2fbf95df1520f021bebe561f3d5622f780004be0
Christopher Sumnicht <Christopher Sumnicht>
2025-09-07 15:37:57 -0700

    Basic page submission rough draft, still not thoroughly tested.

--- static/conway99.ts
+++ static/conway99.ts
@@ -1,5 +1,25 @@
-function conwaySubmit(event) {
+async function conwaySubmit(event) {
+  // Don't go to new page.
   event.preventDefault();
+  const jsonFile: HTMLInputElement | null 
+    = document.getElementById("json") as HTMLInputElement;
+  const file = jsonFile.files![0];
+  const reader = new FileReader();
+  reader.readAsText(file);
+  const result: string = await new Promise((resolve, reject) => {
+    reader.onload = () => {
+      const submission: JSON = JSON.parse(reader.result.toString());
+      resolve(submission.toString())
+    };
+    reader.onerror = (e) => {
+      reject(e)
+    };
+  });
+  const response = await fetch("/conway-submit", {
+    method: "POST",
+    body: result
+  });
+  console.log(await response.json());
 }
 
 document.addEventListener("DOMContentLoaded", function() {
