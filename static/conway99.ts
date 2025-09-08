6b863390b19f1b7f0565ce61774051fa7e60c149
Christopher Sumnicht <Christopher Sumnicht>
2025-09-07 22:29:53 -0700

    Added additional client side validation

--- static/conway99.ts
+++ static/conway99.ts
@@ -1,15 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", conwaySubmit);
+});
+
 async function conwaySubmit(event) {
   // Don't go to new page.
   event.preventDefault();
+  const statusDiv = document.getElementById("status");
   const jsonFile: HTMLInputElement | null 
-    = document.getElementById("json") as HTMLInputElement;
+    = document.getElementById("graph") as HTMLInputElement;
   const file = jsonFile.files![0];
+  if (!file) {
+    statusDiv.textContent = "No JSON file selected!";
+    return;
+  }
   const reader = new FileReader();
   reader.readAsText(file);
   const result: string = await new Promise((resolve, reject) => {
     reader.onload = () => {
-      const submission: JSON = JSON.parse(reader.result.toString());
-      resolve(submission.toString())
+      // Check if valid JSON file.
+      try {
+        const submission: JSON = JSON.parse(reader.result.toString());
+        resolve(submission.toString())
+      } catch (e) {
+        statusDiv.textContent = "Invalid JSON file!";
+	console.error(e);
+	reject("Invalid JSON");
+      }
+      // TODO Check if JSON file meets submission requirements.
     };
     reader.onerror = (e) => {
       reject(e)
@@ -21,8 +39,3 @@ async function conwaySubmit(event) {
   });
   console.log(await response.json());
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("form");
-  form.addEventListener("submit", conwaySubmit);
-});
