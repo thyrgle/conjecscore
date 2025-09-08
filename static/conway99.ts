591cf263109146e80186d07f2ad0b2a22a779e02
Christopher Sumnicht <Christopher Sumnicht>
2025-09-07 23:07:35 -0700

    Added more client side validation

--- static/conway99.ts
+++ static/conway99.ts
@@ -33,6 +33,23 @@ async function conwaySubmit(event) {
       reject(e)
     };
   });
+  try {
+    const submission = JSON.parse(result);
+    let score = 0;
+    for (let i = 0; i < 99; i++) {
+      for (let j = i + 1; j < 99; j++) {
+	const adj1 = new Set(submission[i.toString()]);
+	const adj2 = new Set(submission[j.toString()]);
+	const c = adj1.intersection(adj2).size;
+	const e = +adj2.has(i);
+	score += (c - (2 - e)) * (c - (2 - e));
+        statusDiv.textContent = `You scored ${score}`;
+      }
+    }
+  } catch (e) {
+    statusDiv.textContent = "Could not score JSON file!";
+    console.error(e);
+  }
   const response = await fetch("/conway-submit", {
     method: "POST",
     body: result
