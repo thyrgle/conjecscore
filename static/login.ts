78aafdd98abee6bf4673e9f655bd81af2ea658ee
Christopher Sumnicht <Christopher Sumnicht>
2025-09-09 12:50:52 -0700

    curl workable authentication, still does not quite work with login and register screens.

--- static/login.ts
+++ static/login.ts
@@ -5,4 +5,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 async function login(event) {
   event.preventDefault();
+  const loc = document.location;
+  const url = loc.host + "/auth/jwt/login";
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+	'Content-Type': 'multipart/form-data',
+      },
+      body: JSON.stringify({ 
+	email: email,
+	password: password,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
--- static/register.ts
+++ static/register.ts
@@ -5,4 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
 async function register(event) {
   event.preventDefault();
+  const loc = document.location;
+  const url = loc.host + "/auth/register";
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ 
+	email: email,
+	password: password,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
