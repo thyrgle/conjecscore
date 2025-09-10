616c4760119c7cb4bf6c5cab1562d62e3d54ee13
Christopher Sumnicht <Christopher Sumnicht>
2025-09-10 09:42:12 -0700

    Use secrets now for saving stuff

--- app/users.py
+++ app/users.py
@@ -13,8 +13,10 @@ from fastapi_users.authentication import (
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
 
-
-SECRET = "SECRET"
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+SECRET = os.getenv("SECRET")
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
