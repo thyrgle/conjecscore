3722205d1ba44e267350e51fd76f5a596b8d06c4
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-03-07 17:42:59 -0700

    Safety check for Conway 99

--- app/routers/problems/conway.py
+++ app/routers/problems/conway.py
@@ -2,9 +2,12 @@ def conway_score(graph, n):
     bad_count = 0
     for i in range(n):
         for j in range(i+1, n):
-            c = len(set(graph[str(i)]) & set(graph[str(j)]))
-            e = int(i in graph[str(j)])
-            bad_count += (c - (2 - e)) * (c - (2 - e))
+            try:
+                c = len(set(graph[str(i)]) & set(graph[str(j)]))
+                e = int(i in graph[str(j)])
+                bad_count += (c - (2 - e)) * (c - (2 - e))
+            except IndexError:
+                return None
     return bad_count
 
 
