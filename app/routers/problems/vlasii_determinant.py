d86ce07c8829fc820aa23a0277472991318ce1a4
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-03-12 21:21:35 -0700

    Switched to 16 x 16 for Vlasii Determinant

--- app/routers/problems/vlasii_determinant.py
+++ app/routers/problems/vlasii_determinant.py
@@ -4,16 +4,17 @@ from collections import Counter
 
 
 async def score(mat: [int]):
+    N = 16
     # Not the correct size.
-    if len(mat) != 10 * 10:
+    if len(mat) != N * N:
         return None
 
     # Ensure there are 10 copies of 1, 10:
     c = Counter(mat)
-    for i in range(1, 11):
-        if c[i] != 10:
+    for i in range(1, N + 1):
+        if c[i] != N:
             return None
 
-    np_mat = np.reshape(mat, (10, 10))
+    np_mat = np.reshape(mat, (N, N))
     sym_mat = Matrix(np_mat)
-    return int(sym_mat.det() / (10 ** 6))
+    return int(sym_mat.det() / (10 ** 16))
--- static/vlasii.ts
+++ static/vlasii.ts
@@ -5,24 +5,24 @@ import * as math from 'mathjs';
 export async function score(nums: Decimal[]): Promise<bigint | string> {
   try {
     const entries = nums.map((num) => num.toNumber());
-    if (entries.length != 10 * 10) {
-      return "Not a 10 × 10 matrix!";
+    const N = 16;
+    if (entries.length != N * N) {
+      return "Not a 16 × 16 matrix!";
     }
-    const mat = math.reshape(math.matrix(entries), [10, 10]);
-    const shouldHave = new Array(10).fill(0);
+    const mat = math.reshape(math.matrix(entries), [N, N]);
+    const shouldHave = new Array(N).fill(0);
     const arr = mat.valueOf();
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
         shouldHave[arr[i][j] - 1] += 1;
       }
     }
     for (const val of shouldHave) {
-      if (val != 10) {
-        console.log(val);
-        return "Not all 1, ..., 10 show up exactly 10 times.";
+      if (val != N) {
+        return "Not all 1, ..., 16 show up exactly 16 times.";
       }
     }
-    return math.bigint(Math.floor(math.det(mat) / (10 ** 6)));
+    return math.bigint(Math.floor(math.det(mat) / (10 ** 16)));
   } catch (e) {
     console.error(e);
     return "Could not score CSV file!";
