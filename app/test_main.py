85a596d3cf3758f3d6629dbeb6c3e27cfd8dc5f4
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-08-13 17:38:36 -0700

    Added async to all tests to keep same loop

--- app/test_main.py
+++ app/test_main.py
@@ -6,19 +6,23 @@ from .db import create_db_and_tables
 
 client = TestClient(app)
 
-def test_simple_read_home():
+@pytest.mark.asyncio(loop_scope="session")
+async def test_simple_read_home():
     response = client.get("/")
     assert response.status_code == 200
 
-def test_simple_read_problems():
+@pytest.mark.asyncio(loop_scope="session")
+async def test_simple_read_problems():
     response = client.get("/problems")
     assert response.status_code == 200
 
-def test_simple_read_login():
+@pytest.mark.asyncio(loop_scope="session")
+async def test_simple_read_login():
     response = client.get("/login")
     assert response.status_code == 200
 
-def test_simple_read_register():
+@pytest.mark.asyncio(loop_scope="session")
+async def test_simple_read_register():
     response = client.get("/register")
     assert response.status_code == 200
 
@@ -27,4 +31,3 @@ async def test_simple_read_users():
     await create_db_and_tables()
     response = client.get("/users")
     assert response.status_code == 200
-
