2801bbe6eb658a7b211051af184ee574f64ea97e
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-08-14 13:17:56 -0700

    Simplified user creation code and removed type errors.

--- app/users.py
+++ app/users.py
@@ -1,5 +1,4 @@
 import uuid
-from typing import Optional
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from .dependencies import templates
@@ -23,19 +22,6 @@ assert SECRET is not None, "SECRET must be set"
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
-
-    async def on_after_register(self, user: User, request: Optional[Request] = None):
-        print(f"User {user.id} has registered.")
-
-    async def on_after_forgot_password(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        print(f"User {user.id} has forgot their password. Reset token: {token}")
-
-    async def on_after_request_verify(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        print(f"Verification requested for user {user.id}. Verification token: {token}")
 
 
 async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
